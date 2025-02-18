3c0868e295aea1f9e55fee09786b954da8a6b4bd
Nata-nk <137524429+Nata-nk@users.noreply.github.com>
2025-02-19 01:18:32 +0200

    start

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import Product from './Product'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Products</h1>
+
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
+
+
