d052df7ef6d28244d717d00f014a0c0362ada0b7
Nata-nk <137524429+Nata-nk@users.noreply.github.com>
2025-02-21 02:25:06 +0200

    start

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import BookList from '../Books/Books'
+
+const favBooks = [
+  { id: "id-1", name: "JS for beginners" },
+  { id: "id-2", name: "React basics" },
+  { id: "id-3", name: "React Router overview" }
+];
+
+export default function App() {
+  return (
+    <>
+	  <h1>Books of the week</h1>
+      <BookList books={favBooks} />
+    </>
+  );
+};
+
+
